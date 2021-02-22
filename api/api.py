d0e5f777c084ec013ad78a37410941194d3f33ff
Ezequiel Torres <torreszeaks@gmail.com>
2021-02-21 18:07:14 -0800

    Now checks for multichoice correct answers.

--- api/api.py
+++ api/api.py
@@ -1,8 +1,9 @@
 import time
+import csv_to_json
 from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/time')
+@app.route('/getjson')
 def get_current_time():
-    return {'time': time.time()}
+    return csv_to_json.get_result("Example.csv")
--- src/App.js
+++ src/App.js
@@ -1,24 +1,200 @@
-import logo from './logo.svg';
+import { act } from '@testing-library/react';
+import React, { useState, useEffect, useReducer } from 'react';
 import './App.css';
 
-function App() {
+
+
+  
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "on":
+      const newSelectedValues = [...state.selectedValues]
+      newSelectedValues[action.payload] = !state.selectedValues[action.payload]
+      const newState = {...state, selectedValues: newSelectedValues}
+      return newState
+    default:
+      return newState
+  }
+}
+
+function FlashCard({switchOn, question, answers, flashCardNumber, setUserAnswer}) {
+
+  const handleChangeTextArea = (event) =>{
+    setUserAnswer(event.target.value)
+  }
+
+  const handleChangeCheckBox = (i) => {
+    switchOn(i)
+  }
+
+  const answersComponents = answers.map((ans, i) => {
+    return (
+      <div key={`${i}`}>
+        <input type="checkbox" value={i} onChange={() => handleChangeCheckBox(i)} />
+        <label key={i}>{ans}</label>    
+      </div>
+    )
+
+  })
+  
+  if (answers[flashCardNumber] !== "") {
+    return (
+      <>
+        {question}
+        {answersComponents}
+      </>
+    )
+  }
+  else{
+    return (
+      <>
+        {question}
+        <input type="textarea" id="fname" name="fname" onChange={handleChangeTextArea}/>
+      </>
+    )
+  }
+}
+
+function ShowFlashCards(switchOn, data, flashCardNumber, setUserAnswer){
+  if (data && data.length > flashCardNumber){
+    return (<FlashCard 
+    switchOn={switchOn}
+    flashCardNumber={flashCardNumber} 
+    question={data[flashCardNumber].question} 
+    answers={data[flashCardNumber].answers}
+    setUserAnswer={setUserAnswer} /> );
+  }
+  else{
+    return <em>"Done!"</em>
+  }
+}
+
+
+function FlashCardPage({switchOn, data, flashCardNumber, next, setUserAnswer}){
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    {ShowFlashCards(switchOn, data, flashCardNumber, setUserAnswer)}
+    {data && flashCardNumber < data.length ? <button onClick={next}>Next</button> : null}
+    </>
+  )
+}
+
+function Page({switchOn, data, flashCardNumber, nextFlashCard, setUserAnswer, isCorrect, isAnswer}){
+  if (!isAnswer){
+    return (
+      <div className="App">
+          <header className="App-header">
+          <FlashCardPage switchOn={switchOn} data={data} flashCardNumber={flashCardNumber} next={nextFlashCard} setUserAnswer={setUserAnswer}/>
+          </header>
+      </div>
+    );
+  }
+  else{
+    if (isCorrect){
+      return (
+        <div className="App">
+            <header className="App-header" style={{backgroundColor: "green"}}>
+              {"Correct"}
+              {<button onClick={nextFlashCard}>Next</button>}
+            </header>
+        </div>
+      )
+    }
+    else{
+      return (
+        <div className="App">
+            <header className="App-header" style={{backgroundColor: "#ff6666"}}>
+              {"Incorrect"}
+              {<button onClick={nextFlashCard}>Next</button>}
+            </header>
+        </div>
+      )
+    }
+  }
+}
+
+const searchState = {
+  SEARCH_FOR_ANSWER_IN: 0,
+  SEARCH_FOR_ANSWER_NOT_IN: 1,
+  SEARCH_NEXT: 2,
+  INCORRECT_ANSWER: 3
+}
+
+function App() {
+  const initialState = {
+    selectedValues: [false, false, false, false]
+  };
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  function switchOn(i){
+    dispatch({type: 'on', payload: i})
+  }
+
+  const [flashCardNumber, setFlashCardNumber] = useState(0)
+  const [data, setData] = useState();
+  const [isAnswer, setAnswer] = useState(false)
+  const [userAnswer, setUserAnswer] = useState("")
+  const [isCorrect, setIsCorrect] = useState(false)
+
+
+  const nextFlashCard = () => {
+    if (flashCardNumber >= data.length){
+      isAnswer ? setAnswer(false) : setAnswer(true)
+      return
+    } 
+    if (isAnswer){
+      setAnswer(false)
+      return
+    }
+    else{
+      setAnswer(true)
+    }
+    setFlashCardNumber(flashCardNumber + 1)
+    if (data[flashCardNumber].answers[0].length == 0){
+      if (data[flashCardNumber].right_answer === userAnswer){
+        setIsCorrect(true)
+      }
+      else{
+        setIsCorrect(false)
+      }
+    }
+    else{
+      const correctAnswers = data[flashCardNumber].right_answer.split(",")
+      var currentSearchState
+      var i = 0
+      state.selectedValues[i] ? currentSearchState = searchState.SEARCH_FOR_ANSWER_IN : currentSearchState = searchState.SEARCH_FOR_ANSWER_NOT_IN
+      while (i < state.selectedValues.length) {
+        switch(currentSearchState){
+          case searchState.SEARCH_FOR_ANSWER_IN:
+            correctAnswers.find(element => parseInt(element) === i + 1) ? currentSearchState = searchState.SEARCH_NEXT : currentSearchState = searchState.INCORRECT_ANSWER
+            break
+          case searchState.SEARCH_FOR_ANSWER_NOT_IN:
+            correctAnswers.find(element => parseInt(element) === i + 1) ? currentSearchState = searchState.INCORRECT_ANSWER : currentSearchState = searchState.SEARCH_NEXT
+            break
+          case searchState.SEARCH_NEXT:
+            i += 1
+            state.selectedValues[i] ? currentSearchState = searchState.SEARCH_FOR_ANSWER_IN : currentSearchState = searchState.SEARCH_FOR_ANSWER_NOT_IN
+            break
+          case searchState.INCORRECT_ANSWER:  
+            setIsCorrect(false)
+            return
+        }
+      } 
+      setIsCorrect(true)      
+    }
+  }
+
+  useEffect(() => {
+    if (data == null){
+      fetch('/getjson').then(res => res.json()).then(d => {
+        setData(d)
+      });
+    }
+  }, []);
+
+
+  return (
+    <Page switchOn={switchOn} data={data} flashCardNumber={flashCardNumber} nextFlashCard={nextFlashCard} setUserAnswer={setUserAnswer} isCorrect={isCorrect} isAnswer={isAnswer}/>
   );
 }
 
